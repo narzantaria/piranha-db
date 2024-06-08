5b4198844a6027001dc8e189e14d3260c9e337cc
Nar Zantaria <narzantaria@gmail.com>
2024-06-08 10:56:52 +0300

    version-1.0.20

--- dist/src/dirs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QUERIES_DIR = exports.DATA_DIR = exports.MODELS_DIR = void 0;
-const { dbconfig } = require(`${process.cwd()}/dbconfig`);
-const { dataFolder, modelsFolder, queriesFolder } = dbconfig;
-exports.MODELS_DIR = `${process.cwd()}/${modelsFolder}`;
-exports.DATA_DIR = `${process.cwd()}/${dataFolder}`;
-exports.QUERIES_DIR = `${process.cwd()}/${queriesFolder}`;
--- dist/src/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IJoin = void 0;
-class IJoin {
-}
-exports.IJoin = IJoin;
